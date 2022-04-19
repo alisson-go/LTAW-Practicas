5ac6c31537d45c5ddc9223c7d77d855a214dcd42
alisson-go <79110107+alisson-go@users.noreply.github.com>
2022-04-19 14:51:33 +0200

    p2

---
+++ P2/products.js
@@ -0,0 +1,34 @@
+[
+    {
+        "productos" : [
+            {
+                "salas":[{
+                    "name" : "sala1" ,
+                    "description" : "sala neón" ,
+                    "price" : "150€" ,
+                },
+                {
+                    "name" : "sala2" ,
+                    "description" : "sala deluxe" ,
+                    "price" : "350€" ,
+                }
+                ]
+            },
+            {
+                "equipos":[{
+                    "nombre":""
+                }
+
+                ]
+            },
+            {
+                "articulos":[{
+                    
+                }
+
+                ]
+            },
+
+        ]
+    }
+]
